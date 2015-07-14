8e859323e656b624d48bc86ee68db39fdf9a9cc3
Toaker <ToakerQin@gmail.com>
2015-07-15 01:14:42 +0800

    Soulwolf send to Bintray maven!
    Signed-off-by: Toaker <ToakerQin@gmail.com>

--- sample/src/main/java/net/soulwolf/image/picturelib/sample/adapter/PictureAdapter.java
+++ sample/src/main/java/net/soulwolf/image/picturelib/sample/adapter/PictureAdapter.java
@@ -1,5 +1,4 @@
 /**
- * <pre>
  * Copyright 2015 Soulwolf Ching
  * Copyright 2015 The Android Open Source Project for PictureLib
  *
@@ -14,7 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * </pre>
  */
 package net.soulwolf.image.picturelib.sample.adapter;
 
